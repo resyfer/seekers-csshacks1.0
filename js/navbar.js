0932bb9f7d8ce4159bf9e83f736e2036199298f7
resyfer <palsaurav.2020@gmail.com>
2021-02-03 14:53:40 +0530

    Changed navbar js to add mob and pc funct.;made map.html responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,11 @@
+/* PC */
 function openNav(){
-  $('#navbar-toggle-on').css({
+  $('#navbar-toggle-pc-on').css({
     'left':'30%',
     'opacity':'0',
     'transition-duration': '0.5s',
   });
-  $('#navbar-toggle-off').css({
+  $('#navbar-toggle-pc-off').css({
     'opacity':'100%',
     'transition-duration': '0.5s',
   });
@@ -22,14 +23,14 @@ function openNav(){
     });
   }, 300);
   setTimeout(function() {
-    $("#navbar-toggle-on").css({
+    $("#navbar-toggle-pc-on").css({
       'display':'none',
     });
   }, 500);
 }
 
 function closeNav(){
-  $('#navbar-toggle-on').css({
+  $('#navbar-toggle-pc-on').css({
     'left':'0',
     'display':'block',
     'opacity':'100',
@@ -45,7 +46,62 @@ function closeNav(){
   $('#menu-container').css({
     'opacity':'0',
   });
-  $('#navbar-toggle-off').css({
+  $('#navbar-toggle-pc-off').css({
+    'opacity':'0',
+    'transition-duration': '0.2s',
+  });
+}
+
+
+/* Mobile */
+function openMobNav(){
+  $('#navbar-toggle-mob-on').css({
+    'left':'100%',
+    'opacity':'0',
+    'transition-duration': '0.5s',
+  });
+  $('#navbar-toggle-mob-off').css({
+    'opacity':'100%',
+    'transition-duration': '0.5s',
+  });
+  $('#navbar-container').css({
+    'display':'block',
+    'width':'100%',
+    'transition-duration': '0.5s',
+  });
+  $('#menu-container').css({
+    'display':'block',
+  });
+  setTimeout(function() {
+    $('#menu-container').css({
+      'opacity':'100%',
+    });
+  }, 300);
+  setTimeout(function() {
+    $("#navbar-toggle-mob-on").css({
+      'display':'none',
+    });
+  }, 500);
+}
+
+function closeMobNav(){
+  $('#navbar-toggle-mob-on').css({
+    'left':'0',
+    'display':'block',
+    'opacity':'100',
+    'transition-duration': '0.2s',
+  });
+  $('#navbar-container').css({
+    'width':'0',
+    'transition-duration': '0.2s',
+  });
+  $('#menu-container').css({
+    'display':'none',
+  });
+  $('#menu-container').css({
+    'opacity':'0',
+  });
+  $('#navbar-toggle-mob-off').css({
     'opacity':'0',
     'transition-duration': '0.2s',
   });
