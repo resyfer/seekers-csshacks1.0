a28e82f6f9f6a66adffe112feb291f44708e48a8
resyfer <palsaurav.2020@gmail.com>
2021-02-04 20:13:03 +0530

    Initialized visits.css;added loader to all pages (html/css)

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,28 @@
-/* PC */
 function openNav(){
+  $('#navbar-container').css({
+    'transform':'translate(0, -50%)',
+    'transition-duration':'0.2s',
+  });
+  $('#navbar-toggle-on').css({
+    'opacity':'0',
+    'transition-duration':'0.2s',
+  })
+}
+
+function closeNav(){
+  $('#navbar-container').css({
+    'transform':'translate(-100%, -50%)',
+    'transition-duration':'0.2s',
+  });
+  $('#navbar-toggle-on').css({
+    'opacity':'100%',
+    'transition-duration':'0.2s',
+  })
+}
+
+/* Map.html */
+  /* PC */
+function openMapNav(){
   $('#navbar-toggle-pc-on').css({
     'left':'30%',
     'opacity':'0',
@@ -29,7 +52,7 @@ function openNav(){
   }, 500);
 }
 
-function closeNav(){
+function closeMapNav(){
   $('#navbar-toggle-pc-on').css({
     'left':'0',
     'display':'block',
@@ -54,7 +77,7 @@ function closeNav(){
 
 
 /* Mobile */
-function openMobNav(){
+function openMobMapNav(){
   $('#navbar-toggle-mob-on').css({
     'opacity':'0',
     'transition-duration': '0.5s',
@@ -83,7 +106,7 @@ function openMobNav(){
   }, 500);
 }
 
-function closeMobNav(){
+function closeMobMapNav(){
   $('#navbar-toggle-mob-on').css({
     'display':'block',
     'opacity':'100',
